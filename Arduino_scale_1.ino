0d8d5a158956b9665cc30e5feffc61e722133dbf
Joshua <70960513+JoshuaVlantis@users.noreply.github.com>
2021-03-26 21:24:49 +0200

    Code Clean up

--- Arduino_scale_1.ino
+++ Arduino_scale_1.ino
@@ -8,9 +8,6 @@ int zero = 13;
 float zerosize = 0;
 int state = 1;
 
-
-
-
 const byte hx711_data_pin = 5;
 const byte hx711_clock_pin = 4;
 
@@ -21,19 +18,12 @@ float avg_size = 10.0; // amount of averages for each mass measurement
 
 Q2HX711 hx711(hx711_data_pin, hx711_clock_pin); // prep hx711
 
-
-
-
 void setup() {
   Serial.begin(9600); // prepare serial port
   delay(1000); // allow load cell and hx711 to settle
   pinMode(potset, INPUT);
   pinMode(zero, INPUT);
 
-  
-  
-  
-  
   lcd.begin();                      // initialize the lcd 
 
   // tare procedure
@@ -68,55 +58,43 @@ void setup() {
 }
 
 
-void loop() {
-   
-   
-   int potstate = digitalRead(potset);
-   
-   
-   
-   
-     
-   
-   if (potstate == HIGH)
+void loop() 
+{
+  
+  int potstate = digitalRead(potset);
+
+  if (potstate == HIGH)
    {
    
    float sensorValue = analogRead(A0);
    float setpoint = sensorValue / 8;
    //Serial.println(setpoint,2);
-  
-  
-  lcd.backlight();
-  lcd.setCursor(1,0);
-  lcd.print("  ");
-  lcd.print("Set Grains");
-  lcd.print("                ");
-  lcd.setCursor(1,1);
-  lcd.print("     ");
-  lcd.print(setpoint,1);
-  lcd.print("                ");
-  
-
-   
+    lcd.backlight();
+    lcd.setCursor(1,0);
+    lcd.print("  ");
+    lcd.print("Set Grains");
+    lcd.print("                ");
+    lcd.setCursor(1,1);
+    lcd.print("     ");
+    lcd.print(setpoint,1);
+    lcd.print("                ");
+ 
    }
-   
-   
-   
-   
-   if (potstate == LOW)
-{
+     
+  if (potstate == LOW)
+  {
       
    
    
-   int sensorValue = analogRead(A0);
-   float setpoint = sensorValue / 10;
-   //Serial.println(setpoint,2);
+    int sensorValue = analogRead(A0);
+    float setpoint = sensorValue / 10;
+    //Serial.println(setpoint,2);
      
-  // averaging reading
-  long reading = 0;
-  for (int jj=0;jj<int(avg_size);jj++){
+    // averaging reading
+    long reading = 0;
+    for (int jj=0;jj<int(avg_size);jj++){
     reading+=hx711.read();
-  }
+    }
   reading/=long(avg_size);
   // calculating mass based on calibration and linear fit
   float ratio_1 = (float) (reading-x0);
@@ -124,22 +102,18 @@ void loop() {
   float ratio = ratio_1/ratio_2;
   float mass = y1*ratio;
 
-int zerostate = digitalRead(zero);
+  int zerostate = digitalRead(zero);
   
-    if (zerostate == HIGH)
+  if (zerostate == HIGH)
   {
     zerosize = (mass);
-    
-    
   }
   
-float zeromass = mass - zerosize;
-String szeromass = String(zeromass);
-float fin = szeromass.toFloat();
-float grain = fin * 15.43;
-Serial.println(grain);
-  
-
+  float zeromass = mass - zerosize;
+  String szeromass = String(zeromass);
+  float fin = szeromass.toFloat();
+  float grain = fin * 15.43;
+  Serial.println(grain);
   
   lcd.backlight();
   lcd.setCursor(1,0);
@@ -150,28 +124,5 @@ Serial.println(grain);
   lcd.print("Grains: ");
   lcd.print(grain,2);
   lcd.print("        ");
-
-  int eighty = mass * 0.8;
-  int ninety = mass * 0.9;
-  
-  if (grain < eighty)
-  {
-  char mystr[5] = "1"; //String data
-  Serial.write(mystr,5); //Write the serial data   
-  }
-
-  if (grain >= eighty)
-    {
-        char mystr[5] = "2"; //String data
-        Serial.write(mystr,5); //Write the serial data
-    }
-  
-char test[5] = "hi"; //String data
-        Serial.write(test,7); //Write the serial data
-}
-  
-  
-
-
-
+ }
 }
